f1aa414711d6b69dceb9554977bad94270f4ec99
Some Guy <theteleforce@gmail.com>
2020-04-24 04:14:29 -0400

    added length check to arg parser

--- akrasia.py
+++ akrasia.py
@@ -140,7 +140,7 @@ class Akrasia(discord.Client):
         command_args = []
         current_arg = []
         in_quotes = False
-        for word in command_content[1:]:
+        for word in [w for w in command_content[1:] if len(w) > 0]:
             if word[0] == '"' and len(word) > 2:
                 word = word[1:]
                 in_quotes = True
