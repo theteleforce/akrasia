889fcbcc0c6b09af98b9ca8c2caf080f454c0c31
Some Guy <theteleforce@gmail.com>
2020-05-03 02:40:05 -0400

    backwards compatability

---
+++ alembic/versions/51eb9fa9b6ec_nothing_to_see_here.py
@@ -0,0 +1,22 @@
+"""exists for historical (read: stupid) reasons
+
+Revision ID: 51eb9fa9b6ec
+Revises:
+Create Date: 2020-05-02 17:36:56.623093
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '51eb9fa9b6ec'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
