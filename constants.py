1bcd5aea3f058c8d004e0922df6ae9c402a52e56
Some Guy <theteleforce@gmail.com>
2020-06-24 05:16:21 -0400

    updated help help message

--- constants.py
+++ constants.py
@@ -72,7 +72,7 @@ MAGIC_8_BALL_RESPONSES = [
 CHANGE_STATUS_TIMER = 300
 
 DEFAULT_HELP_DICT = {
-                            "help": "honestly? i have no fuckin idea",
+                            "help": "Summons Cthulu directly behind you.",
                             "echo": "**echo** *[message]*\n"
                             "*echo [channel] [message]*\n"
                             "*Permissions required: bot instance owner*\n"
